78a392609fb4a418be5fb1a462f77c64b9af5aa7
KoNit. <konit.block@protonmail.com>
2024-10-26 17:49:41 +0800

    Create Chinese Version's Readme

---
+++ README_zh.m
@@ -0,0 +1,50 @@
+# 🚀 DApp（去中心化应用）知识总结
+
+欢迎来到 **KoNit's DApp 学习与开发笔记** 仓库！📚 本项目旨在归纳和总结在 DApp 学习和开发过程中获得的知识，包括 DEX、MPC 钱包、HD 钱包、钱包资金归集等多个主题。
+
+如果您有兴趣合作或讨论这些主题，欢迎通过电子邮件联系我们：[konit.block@protonmail.com](mailto:konit.block@protonmail.com)。
+
+---
+
+**语言**：[English](./README.md) | [中文](./README_zh.md)  
+
+---
+
+## 📁 目录
+
+### 钱包 👜
+这里包含了不同类型钱包及其功能的详细文档。您可以从以下列表中选择具体的项目和资源。
+
+- [ ] [**（多签）多重签名钱包**](./wallets/multisignature.md)
+- [ ] [**（MPC）多方计算钱包**](./wallets/mpc.md)
+- [ ] [**（SCW）智能合约钱包**](./wallets/contract_wallet.md)
+- [ ] [**（HD）HD 钱包**](./wallets/hd_wallet.md)
+- [ ] [**更多...**](./wallets/other.md)
+
+---
+
+### 去中心化交易所（DEXs） 🔄
+探索不同 DEX 协议及其功能的资源和知识总结。
+
+- [ ] [**Uniswap**](./dex/uniswap.md)
+- [ ] [**PancakeSwap**](./dex/pancakeswap.md)
+- [ ] [**Curve**](./dex/curve.md)
+- [ ] [**SushiSwap**](./dex/sushiswap.md)
+- [ ] [**Balancer**](./dex/balancer.md)
+- [ ] [**更多协议...**](./dex/other_dex_protocols.md)
+
+---
+
+### 公链 🌐
+深入了解主要公链的技术细节和概念，这些公链被广泛用于去中心化应用中。
+
+- [ ] [**以太坊（Ethereum, ETH）**](./blockchains/ethereum.md)
+- [ ] [**波场（TRON）**](./blockchains/tron.md)
+- [ ] [**Sui**](./blockchains/sui.md)
+- [ ] [**币安智能链（Binance Smart Chain, BSC）**](./blockchains/bsc.md)
+- [ ] [**Polygon (MATIC)**](./blockchains/polygon.md)
+- [ ] [**更多网络...**](./blockchains/other_blockchains.md)
+
+---
+
+欢迎您探索每个主题，并使用这些材料来拓展您对去中心化应用的知识。该仓库将随着新主题和项目的探索不断增长。祝学习与开发愉快！🌟
